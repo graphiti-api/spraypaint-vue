429a3dd74b6a7aa0c748c7b12c419ad306a048a5
KFlash <YOUR EMAIL ADDRESS>
2016-06-26 14:43:11 +0800

    v.0.0.3

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-set -ex
-
-BIN=$PWD/node_modules/.bin
+set -ex -o pipefail
 
 # Transpile TypeScript for the unit tests
 TSCONFIG=./tsconfig.json
@@ -21,7 +19,7 @@ node --harmony ./node_modules/istanbul/lib/cli.js cover --root build/src --repor
 
 # Check for Travis CI and Circle-CI environment
 if [ $(env | grep TRAVIS_JOB_ID ) ] || [ $(env | grep CIRCLECI) ] ; then
-  cat coverage/lcov.info | $BIN/coveralls || echo "Coveralls upload failed"
+  cat coverage/lcov.info | node_modules/.bin/coveralls || echo "Coveralls upload failed"
   # Upload to coveralls.io
-  $BIN/codecov && rm -rf ./coverage
+  ./node_modules/.bin/codecov && rm -rf ./coverage
 fi
