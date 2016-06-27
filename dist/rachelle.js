79ee6728f5329325f66813e761d691f95dc44ea6
Kflash <kflash123@gmail.com>
2016-06-27 21:23:34 +0800

    v.0.2.5

--- dist/rachelle.js
+++ dist/rachelle.js
@@ -1,16 +1,16 @@
 /*
 * rachelle.js
-* @version 0.2.4
+* @version 0.2.5
 * @copyright (c) 2016 - KFlash
 * @license MIT <undefined/blob/master/LICENSE>
 */
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    (global.rachelle = factory());
+    (factory());
 }(this, function () { 'use strict';
 
-    var rachelle = {
+    exports.default = {
         // 'a' should not be covered
         a: function () {
             var uncovered = true;
@@ -18,7 +18,5 @@
         },
         b: function () { return true; }
     };
-
-    return rachelle;
 
 }));
--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -18,13 +18,3 @@ echo "Gathering coverage report..." >&2
 # Generate the coverage report with Istanbul
 node --harmony ./node_modules/istanbul/lib/cli.js cover --root build/src --report lcov --report text \
      ./node_modules/mocha/bin/_mocha -- -R spec --check-leaks ./build/test/**/*.js
-
-# Check for Travis CI
-if [ $(env | grep TRAVIS_JOB_ID ) ] ; then
-  # Push to coveralls.io
-  cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
-  ## Push to codecov
-  ./node_modules/.bin/codecov
-  # Remove the coverage files and the directory
-  rm -rf ./coverage
-fi
