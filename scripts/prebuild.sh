0c2844f027d9a5cdb761067b7518761879413f52
KFlash <kflash123@gmail.com>
2016-06-27 22:44:58 +0800

    Update prebuild.sh

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+# run in subshell to avoid polluting cwd
+(
+
 set -e
 
 # remove the build folder if it does exist
 if [ -d build ]; then
    rm -rf ./build
 fi
+
+./node_modules/.bin/tsc --version
 
 # Transpiling all TypeScript files in project
 # If we allow dead code elimination with TypeScript, we will end up with a nasty looking bundle
@@ -14,8 +19,10 @@ fi
 
 TSCONFIG=./tsconfig.json
 
- $(npm bin)/tsc  \
+echo "> compiling..."
+ ./node_modules/.bin/tsc  \
         --outDir ./build \
         -t es6 \
         --skipLibCheck \
         -p ${TSCONFIG}
+)
