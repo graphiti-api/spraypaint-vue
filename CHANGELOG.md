288ab2a6e16fc61d92d215eafbc332e4b6dcb260
Kflash <kflash123@gmail.com>
2016-06-28 07:48:38 +0800

    v.0.3.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## Changelog
 
+# 0.3.6
+
+- solved issue where Mocha watch task wasn't working
+
 # 0.3.5
 
 - fixed script issue
--- dist/rachelle.js
+++ dist/rachelle.js
@@ -1,6 +1,6 @@
 /*
 * rachelle.js
-* @version 0.3.5
+* @version 0.3.6
 * @copyright (c) 2016 - KFlash
 * @license MIT <undefined/blob/master/LICENSE>
 */
