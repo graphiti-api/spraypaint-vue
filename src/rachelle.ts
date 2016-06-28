d6be4ddf6b06ecb273867af6bc4d832706602983
Kflash <kflash123@gmail.com>
2016-06-28 13:12:28 +0800

    added entry comment

--- src/rachelle.ts
+++ src/rachelle.ts
@@ -1,3 +1,11 @@
+/*
+ * This is the main entry point for your package.
+ *
+ * You can import other modules here, including external packages. When
+ * bundling using rollup you can mark those modules as external and have them
+ * excluded or, if they have a jsnext:main entry in their package.json (like
+ * this package does), let rollup bundle them into your dist file.
+ */
 export default {
    // 'a' should not be covered
   a: (): boolean => {
