85bb05ec200ded7e14d75ed6fc1b7394411724ef
Kflash <kflash123@gmail.com>
2016-06-29 11:22:26 +0800

    v.0.4.1

--- dist/rachelle.js
+++ dist/rachelle.js
@@ -1,6 +1,6 @@
 /*
 * rachelle.js
-* @version 0.4.0
+* @version 0.4.1
 * @copyright (c) 2016 - KFlash
 * @license MIT <undefined/blob/master/LICENSE>
 */
--- test/setup/setup-mocha.js
+++ test/setup/setup-mocha.js
@@ -3,4 +3,3 @@
 global.chai = require('chai');
 global.sinon = require('sinon');
 global.chai.use(require('sinon-chai'));
-global.expect = require('chai').expect;
