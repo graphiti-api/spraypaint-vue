11f5d6d0bd1f882bb877f119a26040991bc3b189
kflash <kflash123@gmail.com>
2016-07-01 23:23:31 +0800

    v.0.5.5

---
+++ dist/rachelle.js
@@ -0,0 +1,24 @@
+/*
+* rachelle.js
+* @version 0.5.4
+* @copyright (c) 2016 - KFlash
+* @license MIT <undefined/blob/master/LICENSE>
+*/
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.rachelle = factory());
+}(this, function () { 'use strict';
+
+    var rachelle = {
+        // 'a' should not be covered
+        a: function () {
+            var uncovered = true;
+            return uncovered;
+        },
+        b: function () { return true; }
+    };
+
+    return rachelle;
+
+}));
--- rollup.config.js
+++ rollup.config.js
@@ -15,7 +15,7 @@ const banner = readFileSync( 'banner.js', 'utf-8' )
   .replace( '${homepage}', pkg.homepage )
 
 export default {
-  entry: `./build/src/${pkg.name}.js`,
+  entry: `./build/${pkg.name}.js`,
   plugins: [
       isProduction ? uglify({}) : {},
       commonjs({ include: './node_modules/**' }),
@@ -25,9 +25,8 @@ export default {
   banner:  banner,
   sourceMap: false,
   moduleName: pkg.name,
-  targets: [ 
+  targets: [
     {  dest: `./dist/${pkg.name}.${isProduction ? 'min.js' : 'js'}`, format: 'umd' }
   ]
 };
 
- 
